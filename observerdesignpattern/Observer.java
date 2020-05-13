2f85cef27095ae93b855020138c75135e594732d
Jadon T Schuler <jtschuler007@yahoo.com>
2020-05-13 17:50:32 -0400

    Added comments

--- observerdesignpattern/Observer.java
+++ observerdesignpattern/Observer.java
@@ -1,5 +1,8 @@
 package observerdesignpattern;
 
+/**
+ * Interface for observers
+ */
 public interface Observer {
     public void update(int strokes, int par);
 }
--- observerdesignpattern/Subject.java
+++ observerdesignpattern/Subject.java
@@ -1,7 +1,23 @@
 package observerdesignpattern;
 
+/**
+ * Interface for subjects
+ */
 public interface Subject {
+    /**
+     * Adds new observers
+     * @param observer the new observer to be added
+     */
     public void registerObserver(Observer observer);
+
+    /**
+     * Removes observers
+     * @param observer the observer to be removed
+     */
     public void removeObserver(Observer observer);
+
+    /**
+     * Updates observers with current data
+     */
     public void notifyObservers();
 }
