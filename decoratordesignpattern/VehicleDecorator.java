f421304b4de4e9c18a6bfd2674a84537c36e5516
Jadon T Schuler <jtschuler007@yahoo.com>
2020-05-18 21:45:59 -0400

    Finished abstract decorator class

---
+++ decoratordesignpattern/VehicleDecorator.java
@@ -0,0 +1,13 @@
+package decoratordesignpattern;
+
+/**
+ * Basics of what a car decoration must do
+ * @author Jadon T Schuler
+ */
+public abstract class VehicleDecorator {
+    /**
+     * Append this particular decoration to the car
+     * @return the newly modified description string
+     */
+    public abstract String toString();
+}
