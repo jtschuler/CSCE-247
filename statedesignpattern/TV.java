222d77078462038358900df7e275feb90b81f1b4
jtschuler <jtschuler007@yahoo.com>
2020-06-16 15:56:31 -0400

    Edited TV

--- statedesignpattern/TV.java
+++ statedesignpattern/TV.java
@@ -1,21 +1,34 @@
 package statedesignpattern;
 
+/**
+ * Class TV. Controlled via the "buttons" (see last 4 methods) on a remote.
+ */
 public class TV {
     private State state;
     private State homeState;
     private State netflixState;
     private State huluState;
 
+    /**
+     * Creates a new TV object and instantiates its state objects
+     */
     public TV() {
         homeState = new HomeState(this);
         netflixState = new NetflixState(this);
         huluState = new HuluState(this);
     }
 
+    /**
+     * Sets the current state of the TV
+     * @param state the new state
+     */
     public void setState(State state) {
         this.state = state;
     }
 
+    /**
+     * Accessors for the different states owned by this TV
+     */
     public State getHomeState(){
         return homeState;
     }
@@ -28,5 +41,26 @@ public class TV {
         return huluState;
     }
 
+    public void pressHomeButton() {
+        state.pressHomeButton();
+    }
 
+    /**
+     * Each method calls the relevant button in the current state
+     */
+    public void pressNetflixButton() {
+        state.pressNetflixButton();
+    }
+
+    public void pressHuluButton() {
+        state.pressHuluButton();
+    }
+
+    public void pressMovieButton() {
+        state.pressMovieButton();
+    }
+
+    public void pressTVButton() {
+        state.pressTVButton();
+    }
 }
